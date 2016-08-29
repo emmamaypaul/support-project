0979c73e050603d78b2cd46c2c2d28143ed6b613
Emma Paul <emmamaypaul@gmail.com>
2016-08-29 16:51:38 -0700

    Sets login routes and error route

--- support_app.rb
+++ support_app.rb
@@ -8,3 +8,20 @@ end
 get '/create_account' do
   erb :create_account
 end
+
+post '/create_account' do
+  # Handle creating the account --> will want to create a class for that
+  # redirects to homepage upon success
+  # otherwise redirects to error page
+end
+
+get '/error_page' do
+  # any errors will cause the user to be redirected to this page to start
+  erb :error_page
+end
+
+get '/login' do
+  # class to handle logging it
+  # upon successful login, redirects to landing page
+  erb :login
+end
